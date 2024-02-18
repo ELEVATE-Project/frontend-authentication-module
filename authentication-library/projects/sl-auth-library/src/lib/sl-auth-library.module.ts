9ebbdf529e779cd2c87a69af9820bc2ffff71673
ivedmohan <vedmohan0@gmail.com>
2024-02-19 01:05:45 +0530

    added routing

--- authentication-library/projects/sl-auth-library/src/lib/sl-auth-library.module.ts
+++ authentication-library/projects/sl-auth-library/src/lib/sl-auth-library.module.ts
@@ -3,8 +3,15 @@ import { SlAuthLibraryComponent } from './sl-auth-library.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { ForgotpasswordComponent } from './components/forgotpassword/forgotpassword.component';
+import { Routes, RouterModule } from '@angular/router';
 
-
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+  {path: 'forgotpassword', component:ForgotpasswordComponent},
+  {path: '', redirectTo:'/login', pathMatch: 'full' },
+  {path: '**', redirectTo:'/login', pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
@@ -14,12 +21,14 @@ import { ForgotpasswordComponent } from './components/forgotpassword/forgotpassw
     ForgotpasswordComponent
   ],
   imports: [
+    RouterModule.forRoot(routes)
   ],
   exports: [
     SlAuthLibraryComponent,
     LoginComponent,
     SignupComponent,
-    ForgotpasswordComponent
+    ForgotpasswordComponent,
+    RouterModule
   ]
 })
 export class SlAuthLibraryModule { }
